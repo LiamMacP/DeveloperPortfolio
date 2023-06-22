29506a071d50f6d62bcd0f3b8e520e4e9dc41a7e
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-06-22 22:13:16 +0100

    Add pathPrefix for deployment
    As the site is hosted not at the root domain but has the repository
    name as a prefix, this change adds the pathPrefix for this. This enables
    the deploment to correctly access static files.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: "/DeveloperPortfolio",
   siteMetadata: {
     title: `Liam MacPherson Developer Portfolio`,
     siteUrl: `https://www.yourdomain.tld`
