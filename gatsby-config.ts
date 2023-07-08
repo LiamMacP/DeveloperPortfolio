fbc86182983eab07f60d8233ae52d2afbe3085b3
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-07-08 17:35:23 +0100

    Remove pathPrefix and change siteUrl
    As the website is now served from the `liammacpherson.dev`apex domain,
    the pathPrefix is no longer required.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,9 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  pathPrefix: "/DeveloperPortfolio",
   siteMetadata: {
     title: `Liam MacPherson Developer Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://liammacpherson.dev`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
